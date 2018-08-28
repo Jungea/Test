c349fd53077c40966890706db9fc72950f5a40a5
Jungea <dmsdo2241@naver.com>
2018-08-28 23:54:47 +0900

    깃 올리기

---
+++ hi/src/hi/Hi.java
@@ -0,0 +1,11 @@
+package hi;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요");
+		System.out.println("깃 올리기 연습 중입니다.");
+	}
+
+}
