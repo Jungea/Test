9c07406760b44758bda8064117b36527d7638d9c
Jungea <dmsdo2241@naver.com>
2018-08-28 23:56:36 +0900

    깃 수정하기 연습

--- hi/src/hi/Hi.java
+++ hi/src/hi/Hi.java
@@ -6,6 +6,8 @@ public class Hi {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요");
 		System.out.println("깃 올리기 연습 중입니다.");
+		System.out.println("깃 수정하기 연습");
+		System.out.println("수정 후 올리기 연습 중입니다.");
 	}
 
 }
